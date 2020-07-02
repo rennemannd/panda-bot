f37cead47346e61c074645935b69431efce2254a
Dakota <rennemannd@gmail.com>
2020-07-02 00:04:26 -0600

    Basic bot setup

---
+++ PandaBot/Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace PandaBot
+{
+    public class Startup
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly string _token;
+        
+        public Startup(string[] args)
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            
+            _token = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText("config.json")).Token;
+        }
+        public static async Task RunAsync(string[] args)
+        {
+            var startup = new Startup(args);
+            await startup.RunAsync();
+        }
+
+        public async Task RunAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, _token);
+            await _client.StartAsync();
+            
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage message)
+        {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 PandaBot
+
+## Setup
+To get this bot started a `config.json` file will need to be created at `/PandaBot`. Inside of this file set the following Json value `"Token": "######"` where the hashes represent a [discord bot token](https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token)
